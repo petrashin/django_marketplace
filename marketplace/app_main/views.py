5845d8bd9be35f613d6eeb984802b8492b90b985
HubPavKul1 <kulpv@mail.ru>
2022-07-19 12:24:41 +0300

    Сделал верхнее меню, футер, базовый шаблон, вьюху для демонстрации базового шаблона; подключил статические файлы; в навигационной панели сделал ссылку для входа в админпанель для админа.

--- marketplace/app_main/views.py
+++ marketplace/app_main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-#feiwnfew
+
+class BaseTemplateView(TemplateView):
+    """ Вьюха для демонстрации базового шаблона """
+    template_name = 'base.html'
+    extra_context = {'title': "Megano"}
+
+
