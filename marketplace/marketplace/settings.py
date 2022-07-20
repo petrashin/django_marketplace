5c7f4523af6bacfb218e2cad615150a0b8b96d5c
kulikov.a <28kulikov@mail.ru>
2022-07-20 09:46:43 +0300

    install django-debug_toolbar

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'app_main.apps.AppMainConfig',
+	'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'marketplace.urls'
@@ -130,3 +132,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
