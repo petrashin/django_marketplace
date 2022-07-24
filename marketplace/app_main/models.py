99bd73ac5cb2fab57247a75b9b22fe51a83853fb
petrashin <petrashin02@bk.ru>
2022-07-24 18:57:12 +0300

    created profile model, added profile model to admin panel

--- marketplace/app_main/models.py
+++ marketplace/app_main/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    avatar_url = models.CharField(max_length=256)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    
