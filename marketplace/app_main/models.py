bedbfeecc11fc2d4c15322c249ca90c239c3138e
n_shankov <69906990>
2022-07-25 17:30:55 +0300

    Added Categories model Updated header.html with using categories objects instead of predefined list Added more context to a temp view for testing

--- marketplace/app_main/models.py
+++ marketplace/app_main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=25)
+    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.DO_NOTHING, related_name="sub")
+    category_icon = models.FileField(upload_to='static/assets/img/icons/departments/')
+
+    def __str__(self):
+        return self.name
