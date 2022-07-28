92f329e9d79a010a1b4e0f43891a56f8bd31bcbd
Vasker <v92prog@gmail.com>
2022-07-28 15:53:19 +0300

    add Role model and integrate it in Profile model

--- marketplace/app_main/admin.py
+++ marketplace/app_main/admin.py
@@ -1,7 +1,11 @@
-from app_main.models import Profile
+from app_main.models import Profile, Role
 from django.contrib import admin
 
 
 @admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Role)
 class ProfileAdmin(admin.ModelAdmin):
     pass
--- marketplace/app_main/models.py
+++ marketplace/app_main/models.py
@@ -2,9 +2,19 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+# Create your models here.
+class Role(models.Model):
+    '''Роли пользователей'''
+    name = models.CharField(verbose_name="Название роли", max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
+    '''Профиль пользователя'''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=12)
     avatar_url = models.CharField(max_length=256)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    role = models.ForeignKey(Role, verbose_name="Роль", on_delete=models.DO_NOTHING)
