a985eb044dac437819fd36bc7a682fcc8ba928b3
petrashin <petrashin02@bk.ru>
2022-08-01 19:14:47 +0300

    added unique email and username for registration

--- marketplace/app_auth/forms.py
+++ marketplace/app_auth/forms.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email",)
+
+    def clean(self):
+       email = self.cleaned_data.get('email')
+       username = self.cleaned_data.get('username')
+       if User.objects.filter(email=email).exists():
+            raise ValidationError("Email exists")
+       elif User.objects.filter(username=username).exists():
+            raise ValidationError("Username exists")
+       return self.cleaned_data
