34665c03c78a1c78408dfca39602efd563683439
petrashin <petrashin02@bk.ru>
2022-08-09 20:09:49 +0300

    created task 19

--- marketplace/app_account/views.py
+++ marketplace/app_account/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from app_users.models import Profile, Image
 
 
-class BaseTemplateView(TemplateView):
-    template_name = 'account.html'
+def account_view(request):
+    profile = Profile.objects.filter(user_id=request.user.id).get()
+    avatar_object = Image.objects.filter(profile_id=profile)
+    data = {
+        "full_name": f"{request.user.first_name} {request.user.last_name}",
+        "avatar": avatar_object[0].avatar
+    }
+    return render(request, "account.html", context=data)
