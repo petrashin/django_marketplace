0ae8b8e585017e7c077138ed10c791b0ff36197f
n_shankov <n_shankov@lesta.group>
2022-08-11 10:29:06 +0300

    added context processor for categories list, added empty category template

--- marketplace/app_shops/views.py
+++ marketplace/app_shops/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from app_goods.models import Category, Product
+from django.core.paginator import Paginator
+from app_goods.models import Product
 
 
 class ShopTemplateView(TemplateView):
@@ -13,19 +13,9 @@ class CatalogTemplateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(CatalogTemplateView, self).get_context_data()
-        context['categories'] = Category.objects.filter(parent_category__isnull=True)
-        products_list = Product.objects.filter(category=kwargs["category_id"])
-        paginator = Paginator(products_list, 8)
-        if 'page' in self.request.GET:
-            page = self.request.GET['page']
-        else:
-            page = 1
-        try:
-            products = paginator.page(page)
-        except PageNotAnInteger:
-            products = paginator.page(1)
-        except EmptyPage:
-            products = paginator.page(paginator.num_pages)
+        page = self.request.GET.get('page', 1)
+        products_qs = Product.objects.filter(category=kwargs["category_id"])
+        products = Paginator(products_qs, 8).get_page(page)
         context['object_list'] = products
         return context
 
@@ -34,8 +24,3 @@ class BaseTemplateView(TemplateView):
     """ Вьюха для демонстрации базового шаблона """
     template_name = 'index.html'
     extra_context = {'title': "Megano"}
-
-    def get_context_data(self, **kwargs):
-        context = super(BaseTemplateView, self).get_context_data()
-        context['categories'] = Category.objects.filter(parent_category__isnull=True)
-        return context
