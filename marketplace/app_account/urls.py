3febdb6547b7ac6f6db5cab0afb22865882e31a4
petrashin <petrashin02@bk.ru>
2022-08-21 16:03:01 +0300

    added history of orders button on account page, changed url to orders history

--- marketplace/app_account/urls.py
+++ marketplace/app_account/urls.py
@@ -4,5 +4,5 @@ from app_account.views import account_view, EditProfile, OrderListView
 urlpatterns = [
     path('', account_view, name='account'),
     path('profile/', EditProfile.as_view(), name='profile'),
-    path('orderhistory/', OrderListView.as_view(), name='orderhistory'),
+    path('orders/', OrderListView.as_view(), name='orderhistory'),
 ]
