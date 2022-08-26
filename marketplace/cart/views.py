6b83dd6ecdc64332397b2ced56f378e2ef4e7379
HubPavKul1 <kulpv@mail.ru>
2022-08-27 00:05:32 +0300

    рефакторинг корзины 260822 возможность выбирать магазин при добавлении в корзину, слияние корзин неавторизованного пользователя и пользователя после авторизации

--- marketplace/cart/views.py
+++ marketplace/cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from app_goods.models import Product
+from app_shops.models import ShopProduct
 from .forms import CartAddProductForm, CartAddProductShopForm, CartShopsForm, CartUpdateQuantityProductForm
 from .models import CartItems
 
@@ -17,15 +18,19 @@ class CartItemsListView(ListView):
         if self.object_list:
             if len(self.object_list) > 1:
                 context['total_cost'] = 0
+                context['old_total_cost'] = 0
                 for item in self.object_list:
-                    item_cost = item.quantity * item.price
-                    context['total_cost'] += item_cost
+                    item.old_price = get_object_or_404(ShopProduct, product=item.product, shop__name=item.shop).price
+                    context['total_cost'] += (item.quantity * item.price)
+                    context['old_total_cost'] += (item.quantity * item.old_price)
                     item.shops_form = CartShopsForm(product=item.product, item_id=item.id)
                     item.update_quantity_form = CartUpdateQuantityProductForm(initial={'quantity': item.quantity,
                                                                                        'item_id': item.id})
             else:
                 item = self.object_list[0]
+                item.old_price = get_object_or_404(ShopProduct, product=item.product, shop__name=item.shop).price
                 context['total_cost'] = item.price * item.quantity
+                context['old_total_cost'] = item.quantity * item.old_price
                 shops = CartItems().get_shops_for_cart_item(product=item.product)
                 shop_list = [shop.shop.name for shop in shops]
                 item.shops = shop_list
@@ -35,16 +40,22 @@ class CartItemsListView(ListView):
 
         return context
 
+    def get_queryset(self):
+        return CartItems().get_cart_items(request=self.request)
+
 
 @require_POST
 def cart_add(request, slug):
+    """ Представление для добавления товара в корзину из детальной страницы товара без выбора продавца """
     cart = CartItems()
     product = get_object_or_404(Product, slug=slug)
+    shop = None
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(request=request,
                  product=product,
+                 shop=shop,
                  quantity=cd['quantity']
                  )
     return redirect('cart_detail')
@@ -52,27 +63,30 @@ def cart_add(request, slug):
 
 @require_POST
 def cart_shop_add(request, slug):
+    """ Представление для добавления товара в корзину с выбранным продавцом """
     cart = CartItems()
     product = get_object_or_404(Product, slug=slug)
-
     form = CartAddProductShopForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(request=request,
                  product=product,
+                 shop=cd['shop'],
                  quantity=cd['quantity']
                  )
     return redirect('cart_detail')
 
 
 def cart_remove(request, **kwargs):
+    """ Представление для удаления товара из корзины """
     item_id = kwargs['pk']
     cart = CartItems()
-    cart.remove_cart_item(request, item_id)
+    cart.remove_cart_item(item_id)
     return redirect('cart_detail')
 
 
 def cart_update_quantity(request, **kwargs):
+    """ Представление для обновления количества товара в корзине """
     item_id = kwargs['pk']
     cart = CartItems()
     cart.update_cart_quantity(request, item_id)
@@ -80,6 +94,7 @@ def cart_update_quantity(request, **kwargs):
 
 
 def cart_update_price(request, **kwargs):
+    """ Представление для обновления магазина и цены товара в корзине """
     item_id = kwargs['pk']
     cart = CartItems()
     cart.update_cart_price(request, item_id)
