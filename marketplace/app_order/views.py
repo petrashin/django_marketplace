a6275182b6228256951c30cf61c1c6c862e594af
kulikov.a <28kulikov@mail.ru>
2022-09-04 10:37:21 +0300

    fix (order_template)

--- marketplace/app_order/views.py
+++ marketplace/app_order/views.py
@@ -11,13 +11,6 @@ from app_goods.models import Product
 from app_shops.models import Shop, ShopProduct
 from cart.models import CartItems
 from django.conf import settings
-from custom_admin.models import DefaultSettings
-from django.db.models import Count, F, Value, Subquery, DecimalField, Exists, ExpressionWrapper, FloatField, Sum
-from django.db import transaction
-from decimal import Decimal
-from app_payment.tasks import handle_payment
-from custom_admin.views import logger
-from app_payment.models import PayStatus
 
 
 class OrderView(View):
@@ -30,121 +23,104 @@ class OrderView(View):
         if request.user.is_authenticated:
             user = User.objects.get(id=request.user.id)
             context['user'] = user
+            cart = CartItems.objects.filter(session_id=request.session.session_key).select_related('product')
+            context['cart'] = cart
             profile = Profile.objects.filter(user_id=request.user.id)
             if profile:
                 profile = Profile.objects.get(user_id=request.user.id)
             else:
-                role = Role.objects.get(name='покупатель')
-                profile = Profile.objects.create(user=user, phone_number='', role=role)
+                profile = Profile.objects.create(user=user, phone_number='')
             context['profile'] = profile
-
-            cart = CartItems.objects.filter(session_id=request.session.session_key, published=True).select_related(
-                'product__discount').annotate(price_discount=ExpressionWrapper(
-                F('price') * (1 - F('product__discount__discount_value') * Decimal('1.0') / 100),
-                output_field=FloatField()), total_sum=Sum(F('price') * F('quantity')),
-                total_sum_with_discount=Sum(F('price_discount') * F('quantity')))
             total_sum = 0
             total_sum_with_discount = 0
             for product in cart:
-                total_sum += product.total_sum
-                if product.total_sum_with_discount:
-                    total_sum_with_discount += product.total_sum_with_discount
-                else:
-                    total_sum_with_discount += product.total_sum
-            context['cart'] = cart
+                shop_id = Shop.objects.get(name=product.shop).id
+                shop_product = ShopProduct.objects.get(shop=shop_id, product=product.product)
+                total_sum += product.price * product.quantity
+                total_sum_with_discount += shop_product.get_discounted_price() * product.quantity
             context['total_sum'] = total_sum
             context['total_sum_with_discount'] = total_sum_with_discount
-
-            return render(request, template_name='order/order.html', context=context)
+            return render(request, template_name='order.html', context=context)
 
     @staticmethod
     def post(request):
+        comment = ''
+        comment_form = OrderCommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.cleaned_data.get('comment')
         data = request.POST
-        comment = data['comment']
         email = data['mail']
         user = User.objects.get(email=email)
         delivery = Delivery.objects.get(title=data['delivery'])
         city = data['city']
         address = data['address']
         pay_method = PayMethod.objects.get(title=data['pay'])
-
+        
+        cart = CartItems.objects.filter(user=user.id).last()
         products = CartItems.objects.filter(session_id=request.session.session_key).select_related('product')
         order_goods = {}
         for product in products:
             order_goods[product.product_id] = product.quantity
-        Order.objects.create(user=user, order_goods=order_goods, delivery=delivery, city=city,
-                             address=address, pay_method=pay_method, order_comment=comment, payment_status='')
-
-        if pay_method.id == 1:
-            return render(request, template_name='order/payment.html', )
-        return render(request, template_name='order/payment_someone.html')
-
-
+        Order.objects.create(user=user, cart=cart, order_goods=order_goods, delivery=delivery, city=city,
+                             address=address, pay_method=pay_method, order_comment=comment, payment_error='')
+        if delivery.id == 1:
+            return render(request, template_name='payment.html', )
+        return render(request, template_name='payment_someone.html')
+    
+    
 class OrderPayment(View):
+    
+    @staticmethod
+    def post(request):
+        card_number = request.POST['numero1']
+        return render(request, template_name='progressPayment.html')
 
-    def post(self, request):
-        card_num = request.POST['numero1'].replace(' ', '')
-        user = request.user
-        profile = Profile.objects.get(user_id=user.id)
-        order = Order.objects.filter(user=user).last()
-        payment_amount = order.get_total_cost()
-        if order.get_total_cost_with_discount():
-            payment_amount = order.get_total_cost_with_discount()
-        handle_payment.delay(order.id, card_num, payment_amount)
-        return render(request, template_name='order/progressPayment.html')
-
-    @transaction.atomic
-    def get(self, request):
-        user = request.user
-        profile = Profile.objects.get(user_id=user.id)
-        order = Order.objects.filter(user=user).last()
-        # cart = CartItems.objects.filter(session_id=request.session.session_key).select_related('product')
-        cart = CartItems.objects.filter(session_id=request.session.session_key, published=True).select_related(
-            'product__discount').annotate(price_discount=ExpressionWrapper(
-            F('price') * (1 - F('product__discount__discount_value') * Decimal('1.0') / 100),
-            output_field=FloatField()), total_sum=Sum(F('price') * F('quantity')),
-            total_sum_with_discount=Sum(F('price_discount') * F('quantity')))
-        if order.payment_status == 'Оплачено':
-            for product in cart:
-                shop = Shop.objects.get(name=product.shop)
-                shop_product = ShopProduct.objects.get(product=product.product, shop=shop.id)
-                if shop_product.quantity - product.quantity >= 0:
-                    shop_product.quantity -= product.quantity
-                    shop_product.save()
-                else:
-                    logger.error(f'Заказ не оформлен. Недостаточное количество товара {product.product}')
-                    #order.payment_status = f'Недостаточное количество товара {product.product}'
-                    pay_status = PayStatus.objects.get(title='недостаточное кол-во товаров')
-                    order.payment_status = pay_status.title
-                    order.save()
-                    return render(request, template_name='order/order_detail.html',
-                                  context={'user': user,
-                                           'profile': profile,
-                                           'cart': cart,
-                                           'order': order
-                                           })
-            logger.info(f'Оформление заказа {order.id} пользователем {user.id}')
-            cart.delete()
-            return render(request, template_name='order/order_detail.html',
-                          context={'user': user,
-                                   'profile': profile,
-                                   'order': order
-                                   })
-        elif order.payment_status:
-            logger.error('Ошибка оплаты')
-            return render(request, template_name='order/order_detail.html', context={'user': user,
-                                                                                     'profile': profile,
-                                                                                     'cart': cart,
-                                                                                     'order': order
-                                                                                     })
-        else:
-            return render(request, template_name='order/progressPayment.html')
-
-
-class OrderRepeat(View):
-
-    def get(self, request):
-        order = Order.objects.filter(user=request.user).last()
-        if order.pay_method_id == 1:
-            return render(request, template_name='order/payment.html', )
-        return render(request, template_name='order/payment_someone.html')
+#         if request.user.is_authenticated:
+#             user = request.user
+#             profile = Profile.objects.get(user_id=user.id)
+#             profile_form = ProfileForm(request.POST, instance=profile)
+#             if profile_form.is_valid():
+#                 user.username = profile_form.cleaned_data.get('username')
+#                 user.first_name = profile_form.cleaned_data.get('first_name')
+#                 user.last_name = profile_form.cleaned_data.get('last_name')
+#                 user.email = profile_form.cleaned_data.get('email')
+#                 user.save()
+#                 profile.phone_number = profile_form.cleaned_data.get('phone_number')
+#                 profile.save()
+#             else:
+#                 print(profile_form.errors)
+#                 if profile_form.errors.get('phone_number'):
+#                     raise ValidationError('неверный номер телефона')
+#                 else:
+#                     raise ValidationError('некорректные данные')
+#         else:
+#             if user_form.is_valid():
+#                 if not user_form.cleaned_data.get('password_1') or not user_form.cleaned_data.get('password_2'):
+#                     raise ValidationError('введите пароли')
+#                 elif user_form.cleaned_data.get('password_1') != user_form.cleaned_data.get('password_2'):
+#                     raise ValidationError('пароли не совпадают')
+#                 else:
+#                     user = user_form.save()
+#                     password = user_form.cleaned_data.get('password_1')
+#                     user.set_password(password)
+#                     user.save()
+#                     user = authenticate(username=user.username, password=password)
+#                     login(request, user)
+#                     phone_number = user_form.cleaned_data.get('phone_number')
+#                     Profile.objects.create(user=user, phone_number=phone_number)
+#             else:
+#                 if user_form.errors.get('username'):
+#                     raise ValidationError('имя пользователя уже существует')
+#                 elif user_form.errors.get('email'):
+#                     raise ValidationError('email уже существует')
+#                 elif user_form.errors.get('phone_number'):
+#                     raise ValidationError('неверный номер телефона')
+#                 else:
+#                     raise ValidationError('некорректные данные')
+#         return render(request, template_name='order_delivery.html', context={'delivery_form': delivery_form})
+#
+#         order_delivery_pay = 0
+#         if order.delivery.title == 'Экспресс-Доставка':
+#             order_delivery_pay = 500
+#         elif order_total_sum < 2000:
+#             order_delivery_pay = 200
