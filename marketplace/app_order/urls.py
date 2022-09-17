b7aa2d7b529583d66172297ccca45007d704e9c4
Artem <28kulikov@mail.ru>
2022-09-17 13:57:12 +0300

    fix ()

--- marketplace/app_order/urls.py
+++ marketplace/app_order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import OrderView, OrderPayment#, OrderDelivery, OrderTotal, OrderCard
+from .views import OrderView, OrderPayment, OrderRepeat
 
 urlpatterns = [
-    path('order', OrderView.as_view(), name='order'),
+    path('cart/order', OrderView.as_view(), name='order'),
     # path('order/delivery', OrderDelivery.as_view(), name='order_delivery'),
-    path('order/payment', OrderPayment.as_view(), name='order_payment'),
+    path('cart/payment', OrderPayment.as_view(), name='order_payment'),
     # path('order/total', OrderTotal.as_view(), name='order_total'),
     # path('order/card', OrderCard.as_view(), name='profile_card'),
+    path('repeat', OrderRepeat.as_view(), name='order_repeat'),
 ]
