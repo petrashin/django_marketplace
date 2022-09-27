8088e45d6c5c4f91f8e935b867472cb13d77f2be
petrashin <petrashin02@bk.ru>
2022-09-27 12:35:59 +0300

    finished command for creating tech specs of products

--- marketplace/app_goods/management/commands/update_tech_specs.py
+++ marketplace/app_goods/management/commands/update_tech_specs.py
@@ -1,4 +1,5 @@
-from random import choice
+import random
+from random import choice, randint, sample
 from app_goods.models import Product
 from django.core.management.base import BaseCommand
 
@@ -6,12 +7,33 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        products = Product.objects.all()
-        list_random_speech = ['privet', 'poka', 'kak dela?']
-        prod_list = list()
-        for i in products:
-            test = choice(list_random_speech)
-            i.technical_specs = {'size': test}
-            prod_list.append(i)
+        """
+        1. Рандомно определяется количество характеристик, которое будет добавлено
+        2. Рандомно выбирается данное количество характеристик из random_specs.keys()
+        3. Рандомно выбирается одно значение из списка random_specs['key']
+        4. Изменный товар добавляется в список для обновления
+        """
 
-        Product.objects.bulk_update(prod_list, fields=['technical_specs'])
+        random_specs = {
+            'size': ['big', 'small', 'medium', 'large'],
+            'rating': [0, 1, 2, 3, 4, 5],
+            'diagonal': [6.1, 6.2, 7.5, 10.8, 13.7],
+            'screen_freq': [60, 75, 120, 144, 240],
+            'country': ['Russia', 'USA', 'China', 'India'],
+            'guarantee': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            'power': [100, 200, 300, 400]
+        }
+
+        products = Product.objects.all()
+        updated_products = []
+
+        for product in products:
+            num_of_specs = randint(1, len(random_specs))
+            specs = random.sample(random_specs.keys(), num_of_specs)
+            specs_result = {}
+            for spec in specs:
+                specs_result[spec] = choice(random_specs[spec])
+            product.technical_specs = specs_result
+            updated_products.append(product)
+
+        Product.objects.bulk_update(updated_products, fields=['technical_specs'])
