7739df497ec430ecd3addc47e363379a1603d622
kulikov.a <28kulikov@mail.ru>
2022-10-04 00:14:22 +0300

    add (order_registration)

--- marketplace/app_order/urls.py
+++ marketplace/app_order/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import OrderView, OrderPayment, OrderRepeat
 
 urlpatterns = [
-    path('cart/order', OrderView.as_view(), name='order'),
+    path('cart/order/', OrderView.as_view(), name='order'),
+    path('cart/order/<str:pk>', OrderView.as_view(), name='order'),
     path('cart/payment', OrderPayment.as_view(), name='order_payment'),
     path('cart/repeat', OrderRepeat.as_view(), name='order_repeat'),
 ]
