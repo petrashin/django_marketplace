7380a2812e16780b2968c3c0bdadebb3ac57ca0d
Vasker <v92prog@gmail.com>
2022-10-12 13:47:50 +0300

    add sales page

--- marketplace/app_goods/urls.py
+++ marketplace/app_goods/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('compare/', CompareGoodsView.as_view(), name='compare'),
     path('compare/<int:pk>/delete/', delete_from_comparison, name='delete_from_comparison'),
     path('compare/<int:pk>/add/', add_to_comparison, name='add_to_comparison'),
+    path('sale/', SaleView.as_view(), name='sale_list'),
 ]
