b8681e45ca692a63a6e6d5f4bb061aa9494da097
petrashin <petrashin02@bk.ru>
2022-10-12 03:14:14 +0300

    deleted unused import

--- marketplace/app_goods/views.py
+++ marketplace/app_goods/views.py
@@ -1,8 +1,7 @@
-from statistics import mean
+from django.shortcuts import render
 
 from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 from django.views import View
