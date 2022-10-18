7a354ffbb2f4fc8b382e756cd8d13daa0182f69d
kulikov.a <28kulikov@mail.ru>
2022-10-18 15:22:00 +0300

    somefix (fix import, add i18n in templates,  somefix)

--- marketplace/app_order/models.py
+++ marketplace/app_order/models.py
@@ -1,7 +1,8 @@
-from django.db import models
-from app_shops.models import ShopProduct, Shop
 from django.contrib.auth.models import User
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from app_shops.models import Shop, ShopProduct
 
 
 class Delivery(models.Model):
--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -11,10 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-z708jiq8d%h54fig$$6v%-u9-f)194@663m^$64h0m=^xay)mm'# os.environ.get("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = True#os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['*']
 
